03647684add57caef23a7cc7e485eb2390a06879
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-06-02 18:49:25 +0000

    test(models): implementa funcao somar com blindagem de tipos e valida tdd

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,6 +1,22 @@
+const calculadora = require("../models/calculadora.js");
+
 test("01 - função teste - Soma", () => {
   const a = 10;
   const b = 7;
   const resultado = a + b;
-  expect(resultado).toBe(30);
+  expect(resultado).toBe(17);
+});
+
+test("02 função da soma - models", () => {
+  const a = 10;
+  const b = 7;
+  const resultado = calculadora.somar(a, b);
+  expect(resultado).toBe(17);
+});
+
+test("03 função da soma - models", () => {
+  const a = 10;
+  const b = "7";
+  const resultado = calculadora.somar(a, b);
+  expect(resultado).toBe(17);
 });
