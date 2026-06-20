2e55f0246a4bf9be6c071ce0798603a9b4192d05
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-06-20 19:00:17 +0000

    feat(api): adiciona monitoramento de max_connections e opened_connections no endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,17 +4,28 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const dataBaseVersionResult = await database.query("SHOW server_version;");
-  const dataBaseVersionResult_ = await database.query("select version();");
-  console.log("Aqui: ", dataBaseVersionResult_.rowCount);
-
   const dataBaseResulValue = dataBaseVersionResult.rows[0].server_version;
-  console.log("Aqui: ", dataBaseResulValue);
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseOpenedConnection = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseOpendConnectionValue = databaseOpenedConnection.rows[0].count;
+  // const pgStatdatabase = await database.query("SELECT * FROM pg_stat_database;");
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: dataBaseResulValue,
+        max_connections: databaseMaxConnectionValue,
+        opened_connections: databaseOpendConnectionValue,
       },
     },
   });
