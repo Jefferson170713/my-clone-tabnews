b8bbf361e72752405500fe845743efebcd965bb1
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-07-24 17:31:01 +0000

    refactor(api): aplica spread operator nas options e ajusta semantica http 200/201

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,28 +3,33 @@ import { runner as migrationRunner } from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dir: join("infra", "migrations"),
+    dryRun: true,
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
     });
-    return response.status(200).json(migrations);
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migrateMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    return response.status(200).json(migrations);
+
+    if (migrateMigrations.length > 0) {
+      return response.status(201).json(migrateMigrations);
+    }
+
+    return response.status(200).json(migrateMigrations);
   }
 
   return response.status(405).end();
