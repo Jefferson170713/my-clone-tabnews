842419e36c9311b23a53822ce0465995b64760a0
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-07-30 02:27:59 +0000

    fix: redeply para bug da vercel

--- infra/migrations/1785378114925_teste-my-migration-2.js
+++ infra/migrations/1785378114925_teste-my-migration-2.js
@@ -1,18 +1,24 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-export const shorthands = undefined;
+const shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+async function up(pgm) {}
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+async function down(pgm) {}
+
+module.exports = {
+  shorthands,
+  up,
+  down,
+};
