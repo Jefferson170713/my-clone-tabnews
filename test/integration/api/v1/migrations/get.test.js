029e2b790842729cb64e5e369a978282b48261b1
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-08-10 16:55:15 +0000

    test(api): injeta orchestrator.waitForAllServices no beforeAll dos testes de integracao

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -1,11 +1,10 @@
-// import database from "../../../../../infra/database.js";
 import database from "infra/database.js";
+import orchestrator from "test/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("GET to api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
@@ -13,6 +12,6 @@ test("GET to api/v1/migrations should return 200", async () => {
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
-  console.log(responseBody);
+  // console.log(responseBody);
   expect(responseBody.length).toBeGreaterThan(0);
 });
