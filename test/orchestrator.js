581be28e999817761e5698e631a6923d170de0b0
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-08-10 16:54:30 +0000

    chore(tests): instala async-retry e cria orquestrador para sincronizar servicos

---
+++ test/orchestrator.js
@@ -0,0 +1,22 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await awaitForWebServer();
+
+  async function awaitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      minTimeout: 1000,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
