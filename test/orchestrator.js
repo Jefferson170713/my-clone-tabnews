8dfbfed5ee55ea1b2188a9aaa63030a0920867d6
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-08-11 16:39:30 +0000

    refactor(orchestrator): inserir modicicação `timeOut`

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -13,8 +13,8 @@ async function waitForAllServices() {
     async function fetchStatusPage(bail, tryNumber) {
       // console.log(tryNumber);
       const response = await fetch("http://localhost:3000/api/v1/status");
-      
-      if(response.status !== 200) {
+
+      if (response.status !== 200) {
         throw Error(`Status page returned ${response.status}`);
       }
     }
